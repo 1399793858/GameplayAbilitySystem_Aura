227d959467b89641d0063cbe3a4a4d15cee0e58f
Marcdi <dixu47822@gmail.com>
2024-10-12 13:10:53 -0700

    Initial Project Files

---
+++ Source/MyAura/MyAuraGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyAuraGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYAURA_API AMyAuraGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
